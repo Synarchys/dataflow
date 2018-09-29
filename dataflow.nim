be7a97719beb87d58cef2b291b032867923f8a05
Ivan Florentin <ivan@sinergetica.com>
2018-09-29 18:04:34 -0400

    testproc working

--- dataflow.nim
+++ dataflow.nim
@@ -2,27 +2,23 @@
 import json, tables, strformat, typetraits
 
 type
-  CallStatus* = object
+  DataContainer*[T] = ref object
+    id*: string  
     flowId*: string
-    docId*: string
+    content*: T
     error*: bool
     code*: int
     message*: string
-  ChangeCB* = proc(s:CallStatus)
-  DataContainer*[T] = ref object
-    id*: string  
-    content*: T
-    status*: CallStatus
   DataFlow*[T] = ref object
     id: string
     options: Table[string, string]
     documents: Table[string, T]
-    subscribers: seq[ChangeCB]
+    subscribers: seq[proc(d: DataContainer[T]){.nimcall.}]
 
 proc createFlow*[T](id: string, options: Table[string, string]): DataFlow[T] =
   result = DataFlow[T](id: id, options: options)
   result.documents = initTable[string, T]()
-  result.subscribers = newSeq[proc(s: CallStatus)]()
+  result.subscribers = @[]
   
 proc `$`*[T](flow: DataFlow[T]): string =
   let typename = T.name
@@ -32,43 +28,59 @@ proc `$`*[T](flow: DataFlow[T]): string =
 
 proc `$`*[T](d:DataContainer[T]): string =
   let typename = T.name
-  result = fmt"DataType: {typename}, ID: {d.id}, Error: {d.status.error}, Code: {d.status.code}, Message: {d.status.message}  "
+  result = fmt"DataType: {typename}, ID: {d.id}, Error: {d.error}, Code: {d.code}, Message: {d.message} Flow: {d.flowId} "
 
-proc callSubscribers[T](flow: DataFlow[T], s: CallStatus) =
+proc callSubscribers[T](flow: DataFlow[T], d: DataContainer) =
   for cb in flow.subscribers:
-    cb(s)
+    echo "running"
+    #cb(d)
     
-proc put*[T](flow: DataFlow[T], doc: var DataContainer[T], cb: proc(d: DataContainer[T])) = 
-  if flow.documents.hasKey(doc.id):
-    flow.documents[doc.id] = doc.content
-    doc.status = CallStatus(code: 0, message: "changed")
+proc put*[T](flow: DataFlow[T], d: var DataContainer[T], cb: proc(d: DataContainer[T])) = 
+  if flow.documents.hasKey(d.id):
+    flow.documents[d.id] = d.content
+    d.code =  0
+    d.message =  "changed"
+    d.flowId = flow.id
   else:
-    flow.documents.add(doc.id, doc.content)
-    doc.status = CallStatus(code: 201, message: "inserted", docId: doc.id)
-  flow.callSubscribers(doc.status)
-  cb(doc)
+    flow.documents.add(d.id, d.content)
+    d.code = 201
+    d.message = "inserted"
+    d.flowId = flow.id
+  flow.callSubscribers(d)
+  cb(d)
   
 proc seek*[T](flow: DataFlow[T], id: string, cb: proc(d: DataContainer[T])) =
   var d = DataContainer[T]()
   if flow.documents.hasKey(id):
     d.content = flow.documents[id]
     d.id = id
-    d.status = CallStatus(code: 200,message: "ok", docId: id)
+    d.code = 200
+    d.message = "ok"
+    d.flowId: flow.id
   else:
-    d.status = CallStatus(error: true, code: 404, message: "Document not found", docId: id)
+    d.error = true
+    d.code =  404
+    d.message = "Document not found"
+    d.flowId =  flow.id
   cb(d)
 
 proc evict*[T](flow: DataFlow[T], id: string, cb: proc(d: DataContainer[T])) =
   var d = DataContainer[T](id:id)
   if flow.documents.hasKey(id):
     flow.documents.del(id)
-    d.status = CallStatus(code: 0, message: "deleted", docId: id, flowId: flow.id)
+    d.code = 0
+    d. message = "deleted"
+    d.id = id
+    d.flowId = flow.id
   else:
     d.id = id
-    d.status = CallStatus(error: true, code: 404, message: "Document not found", docId: id)
+    d.error = true
+    d.code = 404
+    d.message = "Document not found"
+    d.flowId: flow.id
 
-proc subscribe*[T](flow: DataFlow[T], cb: proc(id: string)) =
-  var d = DataContainer[T]()
+proc subscribe*[T](flow: var DataFlow[T], cb: proc(d: DataContainer[T])) =
+  #var d = DataContainer[T]()
   flow.subscribers.add(cb)
     
     
--- testFlow.nim
+++ testFlow.nim
@@ -1,18 +1,18 @@
 import json, tables , sugar
 import dataflow
 
-let df = createFlow[JsonNode]("myFlow", {"option1": "value1"}.toTable)
+var df = createFlow[JsonNode]("myFlow", {"option1": "value1"}.toTable)
 echo $df
 var d = DataContainer[JsonNode](id: "1", content: %*{"name": "asdqwe"})
 
-df.put(d, proc(e:DataContainer[JsonNode]) = echo $e)
+df.put(d, proc(d: DataContainer[JsonNode]) = echo $d)
 
-proc changes(id: string) =
+proc changes(d: DataContainer[JsonNode]){.nimcall.} =
   echo " there are changes"
-  df.seek(id, proc(e:DataContainer[JsonNode]) = echo $e)
+  echo $d
+  #df.seek(id, proc(e:DataContainer[JsonNode]) = echo $e)
 
 df.subscribe(changes)
-
 
 df.seek("1", proc(e:DataContainer[JsonNode]) = echo $e)
 df.seek("2", proc(e:DataContainer[JsonNode]) = echo $e)
