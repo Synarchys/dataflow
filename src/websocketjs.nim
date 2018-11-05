b2cb00cc5b499f2f7e26783cf6aff86becd398a1
Ivan Florentin <ivan@synarchys.com>
2018-11-05 15:32:30 -0300

    minimal websocket implementation

---
+++ src/websocketjs.nim
@@ -0,0 +1,5 @@
+import jsffi
+
+## A simple wrapper over the web socket implementation on the browser
+## https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
+proc newWebSocket(url, protocol: cstring): JsObject {.importcpp: "new WebSocket(@)".}
